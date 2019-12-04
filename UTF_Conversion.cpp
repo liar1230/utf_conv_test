e1c04f5a7c0caabbe9799195d1c0824fc259b156
liar1230 <49141008+liar1230@users.noreply.github.com>
2019-12-05 00:34:03 +0900

    add comments and remove dead codes

--- UTF_Conversion.cpp
+++ UTF_Conversion.cpp
@@ -16,14 +16,10 @@ std::string utf16_to_utf8(std::wstring utf16_string)
 int main()
 {
 	// UTF-8 data. The character U+1d10b, musical sign segno, does not fit in UCS2
-	std::string utf8 = u8"z\u6c34\U0001d10b";
-	/* C:\Users\dlska\Desktop\테스트.txt (2019-12-05 오전 12:23:13)
-   StartOffset(h): 00000000, EndOffset(h): 0000000C, 길이(h): 0000000D */
-
 	unsigned char rawData[13] = {
 		0xEF, 0xBB, 0xBF, 0x74, 0x65, 0x73, 0x74, 0xEC, 0x95, 0x88, 0xEB, 0x85,
 		0x95
-	};
+	}; //test안녕
 
 	std::string strA(rawData, rawData + sizeof(rawData));
 	std::wstring utf16;
