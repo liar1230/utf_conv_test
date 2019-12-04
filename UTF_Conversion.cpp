b01029ea38b50dce8ab962ebf2fe66a15040b3dc
liar1230 <49141008+liar1230@users.noreply.github.com>
2019-12-05 01:15:21 +0900

    support FileRead

--- UTF_Conversion.cpp
+++ UTF_Conversion.cpp
@@ -1,7 +1,10 @@
-﻿#include <iostream>
+#include <iostream>
+#include <fstream>
 #include <string>
 #include <locale>
 #include <codecvt>
+
+using namespace std;
 
 std::wstring utf8_to_utf16(std::string utf8_string)
 {
@@ -13,18 +16,46 @@ std::string utf16_to_utf8(std::wstring utf16_string)
 	return std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t>{}.to_bytes(utf16_string);
 }
 
-int main()
+int main(int argc, char** argv)
 {
-	// UTF-8 data. The character U+1d10b, musical sign segno, does not fit in UCS2
+	if (argc != 2) {
+		cout << "\nUsage: docsample filename\n";
+		return 0;
+	}
+	const char* test_file_path = argv[1];
+	std::ifstream in(test_file_path);
+	std::string inputStr;
+
+	if (in.is_open()) {
+		// 위치 지정자를 파일 끝으로 옮긴다.
+		in.seekg(0, std::ios::end);
+
+		// 그리고 그 위치를 읽는다. (파일의 크기)
+		int size = in.tellg();
+
+		// 그 크기의 문자열을 할당한다.
+		inputStr.resize(size);
+
+		// 위치 지정자를 다시 파일 맨 앞으로 옮긴다.
+		in.seekg(0, std::ios::beg);
+
+		// 파일 전체 내용을 읽어서 문자열에 저장한다.
+		in.read(&inputStr[0], size);
+		std::cout << inputStr << std::endl;
+	}
+	else {
+		std::cout << "파일을 찾을 수 없습니다!" << std::endl;
+	}
+
 	unsigned char rawData[13] = {
 		0xEF, 0xBB, 0xBF, 0x74, 0x65, 0x73, 0x74, 0xEC, 0x95, 0x88, 0xEB, 0x85,
 		0x95
-	}; //test안녕
+	};
 
 	std::string strA(rawData, rawData + sizeof(rawData));
 	std::wstring utf16;
 
-	utf16 = utf8_to_utf16(strA);
+	utf16 = utf8_to_utf16(inputStr);
 
 	return 0;
 }
